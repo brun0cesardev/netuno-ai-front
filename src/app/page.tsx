93346745d705a6c98cefd655a9695bfe3f4cf24b
bruno-sisplan <bruno.sena@sisplansistemas.com.br>
2023-07-28 15:14:34 -0300

    criado conexao com back end e rota de pergunta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Chat } from "@/components/chat";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
+    <div className="flex min-h-screen bg-slate-200 items-center justify-center">
       <Chat/>
     </div>
   )
